66737cdc4233bfd5c2bc38411b0eefd159fff9c3
thomas <thomaselmoleung@gmail.com>
2020-05-16 14:39:37 -0400

    created a switch for satellite in setting dialog

--- lib/settingsDialog.dart
+++ lib/settingsDialog.dart
@@ -6,37 +6,53 @@ class SettingDialog extends StatefulWidget {
 }
 
 class _SettingDialogState extends State<SettingDialog> {
+  bool showSat = false;
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: Colors.transparent,
       content: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            height: 400,
-            width: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
+        child: Container(
+          height: 400,
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                child: Text(
                   "Setting",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                 ),
-                const Divider(
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: const Divider(
                   color: Colors.white38,
                 ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      children: <Widget>[],
-                    ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: <Widget>[
+                      SwitchListTile(
+                        title: const Text('Show Satellite'),
+                        value: showSat,
+                        onChanged: (bool value) {
+                          setState(() {
+                            showSat = value;
+                          });
+                        },
+                        secondary: const Icon(Icons.airplanemode_active),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         ),
       ),
