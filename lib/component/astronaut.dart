07ce1bf7d2ee12aefe2dfe0ba977c1f29090d2d1
Thomas <32366586+Thomas-Leung@users.noreply.github.com>
2020-06-30 01:08:42 -0400

    refined and finalized astronaut's animation

--- lib/component/astronaut.dart
+++ lib/component/astronaut.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
@@ -9,23 +10,56 @@ class Astronaut extends StatefulWidget {
 }
 
 class _AstronautState extends State<Astronaut> {
-  bool tap = false;
-  var randomNum = 0;
+  bool _animating = false;
+  Timer _timer;
+  Map<String, int> _astAnimeTime = {
+    "flash": 1,
+    "float": 30,
+    "phone": 20,
+    "walk": 16
+  };
+
+  void startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    int _start = _astAnimeTime[widget.astAnime];
+    _timer = new Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(() {
+        if (_start < 1) {
+          _animating = false;
+          timer.cancel();
+        } else
+          _start--;
+      }),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_timer != null) {
+      _timer.cancel();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          setState(() {
-            tap = !tap;
-          });
-        },
-        child: FlareActor(
-          'assets/Astronaut.flr',
-          fit: BoxFit.fitWidth,
-          animation: tap ? widget.astAnime : "idle",
+    return IgnorePointer(
+      ignoring: _animating,
+      child: Container(
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            setState(() {
+              _animating = true;
+              startTimer();
+            });
+          },
+          child: FlareActor(
+            'assets/Astronaut.flr',
+            fit: BoxFit.fitWidth,
+            animation: _animating ? widget.astAnime : "idle",
+          ),
         ),
       ),
     );
